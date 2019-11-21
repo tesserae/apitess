16bd14d52fca86be0482bc213a20b3b246c3f49a
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2019-11-21 15:08:15 -0500

    Better error message for bad parameter
    Requires tesserae-v5 to be updated as well
    (at least 40927ee880a8efcb8d1d95cb00933535b70b42a1)

--- tests/test_parallels.py
+++ tests/test_parallels.py
@@ -56,3 +56,51 @@ def test_search(populated_app, populated_client):
                 results_id=results_id)
     response = populated_client.get(retrieve_endpoint)
     assert response.status_code == 200
+
+
+def test_bad_feature_search(populated_app, populated_client):
+    bad_feature = 'DEADBEEF'
+
+    # request a bad search
+    with populated_app.test_request_context():
+        populated_app.preprocess_request()
+        found_texts = flask.g.db.find(tesserae.db.entities.Text.collection)
+        submit_endpoint = flask.url_for('parallels.submit_search')
+    headers = werkzeug.datastructures.Headers()
+    headers['Content-Type'] = 'application/json; charset=utf-8'
+    search_query = {
+        'source': {'object_id': str(found_texts[0].id), 'units': 'line'},
+        'target': {'object_id': str(found_texts[1].id), 'units': 'line'},
+        'method': {'name': 'original',
+            # !!! bad feature
+            'feature': bad_feature,
+            'stopwords': ['et', 'qui', 'quis'],
+            'freq_basis': 'corpus',
+            'max_distance': 6,
+            'distance_basis': 'frequency'}}
+    response = populated_client.post(submit_endpoint,
+            data=json.dumps(search_query), headers=headers)
+    assert response.status_code == 201
+    assert 'Location' in response.headers
+    results_id = response.headers['Location'].split('/')[-2]
+
+    # make sure bad feature was caught
+    with populated_app.test_request_context():
+        populated_app.preprocess_request()
+        status_endpoint = flask.url_for('parallels.retrieve_status',
+                results_id=results_id)
+    response = populated_client.get(status_endpoint)
+    while response.status_code == 404:
+        response = populated_client.get(status_endpoint)
+        time.sleep(1)
+    assert response.status_code == 200
+    data = response.get_json()
+    status = data['status']
+    while status != tesserae.db.entities.ResultsStatus.FAILED:
+        response = populated_client.get(status_endpoint)
+        data = response.get_json()
+        status = data['status']
+        time.sleep(1)
+    assert status == tesserae.db.entities.ResultsStatus.FAILED
+    assert 'message' in data
+    assert f'"{bad_feature}"' in data['message']
