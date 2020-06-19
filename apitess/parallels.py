e4fd46abfd0bc2b89fba9ccc08a98b458a4e65b8
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2020-06-19 13:47:29 -0400

    Breaking change: PageOptions
    Need to update v5 library for this to work.

--- apitess/parallels.py
+++ apitess/parallels.py
@@ -12,7 +12,7 @@ import tesserae.db.entities
 from tesserae.matchers.text_options import TextOptions
 import tesserae.utils.search
 import apitess.errors
-from apitess.utils import fix_id, common_retrieve_status
+from apitess.utils import common_retrieve_status
 
 bp = flask.Blueprint('parallels', __name__, url_prefix='/parallels')
 
@@ -63,12 +63,14 @@ def submit_search():
         return apitess.errors.error(
             400,
             data=received,
-            message='The following errors were found in source and target unit specifications:\n{}'.format('\n\t'.join(errors)))
+            message=('The following errors were found in source and target '
+                     'unit specifications:\n{}'.format('\n\t'.join(errors))))
 
     source_object_id = source['object_id']
     target_object_id = target['object_id']
-    results = flask.g.db.find(tesserae.db.entities.Text.collection,
-            _id=[ObjectId(source_object_id), ObjectId(target_object_id)])
+    results = flask.g.db.find(
+        tesserae.db.entities.Text.collection,
+        _id=[ObjectId(source_object_id), ObjectId(target_object_id)])
     results = {str(t.id): t for t in results}
     errors = []
     if source_object_id not in results:
@@ -79,7 +81,9 @@ def submit_search():
         return apitess.errors.error(
             400,
             data=received,
-            message='Unable to find the following object_id(s) among the texts in the database:\n\t{}'.format('\n\t'.join(errors)))
+            message=('Unable to find the following object_id(s) among the '
+                     'texts in the database:\n\t{}'.format(
+                         '\n\t'.join(errors))))
     source_text = results[source_object_id]
     target_text = results[target_object_id]
 
@@ -103,7 +107,8 @@ def submit_search():
         return apitess.errors.error(
             400,
             data=received,
-            message='The specified method is missing the following required key(s): {}'.format(', '.join(missing)))
+            message=('The specified method is missing the following required '
+                     'key(s): {}'.format(', '.join(missing))))
 
     results_id = tesserae.utils.search.check_cache(
         flask.g.db, source, target, method)
@@ -135,13 +140,13 @@ def submit_search():
                 'max_distance': method['max_distance'],
                 'distance_basis': method['distance_basis'],
                 'min_score': 0
-        })
+            })
     except queue.Full:
         return apitess.error.error(
             500,
             data=received,
             message=('The search request could not be added to the queue. '
-                'Please try again in a few minutes'))
+                     'Please try again in a few minutes'))
     return response
 
 
@@ -178,12 +183,18 @@ def retrieve_results(results_id):
         return response
 
     params = results_status_found[0].parameters
+    page_options = tesserae.utils.search.PageOptions(
+        sort_by=flask.request.args.get('sort_by'),
+        sort_order=flask.request.args.get('sort_order'),
+        per_page=flask.request.args.get('per_page'),
+        page_number=flask.request.args.get('page_number')
+    )
 
     response = flask.Response(
         response=gzip.compress(flask.json.dumps({
             'data': params,
             'parallels': tesserae.utils.search.get_results(
-                flask.g.db, results_id)
+                flask.g.db, results_id, page_options)
         }).encode()),
         mimetype='application/json',
     )
