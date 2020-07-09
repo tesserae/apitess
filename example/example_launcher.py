45e04f294e2a61ad9ec517d275472332c2ae724f
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2020-07-09 16:51:19 -0400

    Updated example_launcher (#39)

--- example/example_launcher.py
+++ example/example_launcher.py
@@ -1,17 +1,23 @@
 """Example script for launching under WSGI"""
 import atexit
+from multiprocessing import freeze_support
 import signal
 
 import apitess
 from tesserae.utils.coordinate import JobQueue
+from tesserae.utils.ingest import IngestQueue
+
 
 # Give app chance to clean up when signal is sent
 def raise_exit(*args):
     raise SystemExit()
 
+
 for sig in [signal.SIGHUP, signal.SIGINT, signal.SIGTERM]:
     signal.signal(sig, raise_exit)
 
+
+freeze_support()
 db_config = {
     'MONGO_HOSTNAME': 'localhost',
     'MONGO_PORT': 27017,
@@ -29,7 +35,9 @@ db_cred = {
 }
 
 a_searcher = JobQueue(5, db_cred)
+ingest_queue = IngestQueue(db_cred)
 
 atexit.register(a_searcher.cleanup)
+atexit.register(ingest_queue.cleanup)
 
-app = apitess.create_app(a_searcher, db_config)
+app = apitess.create_app(a_searcher, ingest_queue, db_config)
